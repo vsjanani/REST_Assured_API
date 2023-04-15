24e1a2007bb19e875d199cd1d20a1a4ff2ffbe76
vsjanani <jansree90@gmail.com>
2023-04-14 22:22:53 -0700

    learning

---
+++ REST_API_Demo/src/Basics.java
@@ -0,0 +1,12 @@
+import io.restassured.RestAssured;
+
+public class Basics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		RestAssured.baseURI = "https://rahulshettyacademy.com";
+		RestAssured.given().
+
+	}
+
+}
